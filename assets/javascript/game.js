f0cdee4b38d470ab79383aa6625673b881d7b23b
iamtheluckyest <annalisa.alden@gmail.com>
2017-07-31 01:30:43 -0400

    attack sequence, loss condition added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,40 +15,52 @@ $(document).ready(function() {
 		azula : new makeChar('Azula', 'assets/images/azula.png', 70, 35, 20)
 	};
 
-	var charArray = Object.keys(characters); // array of character objects
-	var remainingChars = charArray;
+	var charArray; // array of character objects
+	var remainingChars;
 	var hero; // character chosen by user
 	var opponent; 
 	var nextChar; //used for generating divs with characters in them; passed into displayChar()
 
 
 /********************************************
-	DISPLAY CHARACTER
+	DISPLAY CHARACTER(S)
 ********************************************/
-	for (i=0; i<charArray.length; i++) {
-		$("#instructions").html("Choose your character.");
-		$(".row2").append(displayChar(characters[charArray[i]].img, characters[charArray[i]].name, characters[charArray[i]].hp, characters[charArray[i]].attack, "pickHero"));
+	startGame();
+
+	function startGame(){
+		// For restarting game
+		$(".row2").empty();
+		$(".row3").empty();
+		$("#playAgain").css("display", "none");
+
+		// Set up characters
+		charArray = Object.keys(characters)
+		remainingChars = charArray;
+		for (i=0; i<charArray.length; i++) {
+			$("#instructions").html("Choose your character.");
+			$(".row2").append(displayChar(characters[charArray[i]].img, characters[charArray[i]].name, characters[charArray[i]].hp, characters[charArray[i]].attack, "pickHero"));
+		}
 	}
 
 	function displayChar(displayImg, displayName, displayHp, displayAttack, round){
 		var displayBlock = $("<div>");
 		displayBlock.addClass("charBox").addClass(round);
 		displayBlock.attr("id", displayName.toLowerCase());
-		displayBlock.html('<img src="' + displayImg + '" alt="' + displayName + '"> <h3>' + displayName +'</h3><p>Health: ' + displayHp + "<br>Attack: " + displayAttack + "</p>");
+		displayBlock.html('<img src="' + displayImg + '" alt="' + displayName + '"> <h3>' + displayName +'</h3><p>Health: ' + displayHp + '<br>Attack: ' + displayAttack + '</p>');
 		return displayBlock;
 	};
 
 /********************************************
 	CHOOSE CHARACTER
 *********************************************/
-	$(".pickHero").on("click", function(){
+	$(document).on("click", ".pickHero", function(){
 		hero = characters[$(this).attr("id")];
 		$(".row2").empty();
 		$(".row2").append(displayChar(hero.img, hero.name, hero.hp, hero.attack, "hero"));
 		remainingChars.splice(remainingChars.indexOf(hero.name.toLowerCase()), 1);
+		$("#instructions").html("Choose your opponent.");
 		for (i=0; i<remainingChars.length; i++) {
-			$("#instructions").html("Choose your opponent.");
-			$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].attack, "pickOpp"));
+			$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "pickOpp"));
 		}
 		//$(this).blur(); //doesn't seem to affect anything
 	});
@@ -56,15 +68,73 @@ $(document).ready(function() {
 /********************************************
 	CHOOSE OPPONENT
 *********************************************/
-	$(".pickOpp").on("click", function(){
-		console.log("hi");
+	$(document).on("click", ".pickOpp", function(){
 		opponent = characters[$(this).attr("id")];
 		$(".row3").empty();
-		$(".row2").append(displayChar(opponent.img, opponent.name, opponent.hp, opponent.attack, "opponent"));
+		$(".row2").append("<h3 id='vs'>VS</h3>");
+		$(".row2").append(displayChar(opponent.img, opponent.name, opponent.hp, opponent.counterAttack, "opponent"));
 		remainingChars.splice(remainingChars.indexOf(opponent.name.toLowerCase()), 1);
+		$("#instructions").html("Attack your opponent!");
 		for (i=0; i<remainingChars.length; i++) {
-			$("#instructions").html("Attack your opponent!");
-			$(".row4").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].attack, "nextOpp"));
+			$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "nextOpp"));
 		}
+		$("#attackBtn").css("display", "inline-block");
 	});
+
+/********************************************
+	ATTACK OPPONENT
+********************************************/
+	$(document).on("click", "#attackBtn", function(){
+		//Attack and change stats
+		opponent.hp -= hero.attack;
+		hero.hp -= opponent.counterAttack;
+		hero.attack += characters[hero.name.toLowerCase()].attack;
+		console.log("hi");
+
+		// Reprint with new stat values
+		$(".row2").empty();
+		$(".row2").append(displayChar(hero.img, hero.name, hero.hp, hero.attack, "hero"));
+		$(".row2").append("<h3 id='vs'>VS</h3>");
+		$(".row2").append(displayChar(opponent.img, opponent.name, opponent.hp, opponent.counterAttack, "opponent"));
+		console.log("hi2")
+
+
+		// Lose condition
+		if (hero.hp<=0) {
+			$("#instructions").html(hero.name + " has no health left! You lose.");
+			$("#attackBtn").css("display", "none");
+			$("#playAgain").css("display", "inline-block");
+		}
+		else if (opponent.hp <= 0) {
+			$("#instructions").html(opponent.name + " has no health left! You win this round.<br>Pick a new opponent.");
+			$("#attackBtn").css("display", "none");
+			$(".row3").empty();
+			$(".opponent").remove();
+			$("#vs").remove();
+			for (i=0; i<remainingChars.length; i++) {
+				$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "pickOpp"));
+			}
+			console.log(remainingChars[0]);
+			console.log(opponent.hp);
+
+			// //Win condition
+			// if (remainingChars[0] ==='undefined' && opponent.hp <=0){
+			// 	$("#instructions").html("You have defeated all opponents! You win!");
+			// 	$("#attackBtn").css("display", "none");
+			// 	$("#playAgain").css("display", "inline-block");
+			// }
+		}
+
+	});
+
+/********************************************
+	RESTART GAME
+********************************************/
+	$(document).on("click", "#playAgain", function(){
+		startGame();
+	});
+
+
+
+
 });
