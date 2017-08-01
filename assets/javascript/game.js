72218e178c2bc28f64c41a62e5a6f5b9d37c4ea1
iamtheluckyest <annalisa.alden@gmail.com>
2017-07-31 22:45:30 -0400

    completed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,20 +6,14 @@ $(document).ready(function() {
 		this.attack = a;
 		this.counterAttack = c;
 	}
-	var characters = {
-		aang : new makeChar('Aang', 'assets/images/aang.png', 80, 30, 15),
-		katara : new makeChar('Katara', 'assets/images/katara.png', 50, 20, 10),
-		sokka : new makeChar('Sokka', 'assets/images/sokka.png', 100, 20, 15),
-		zuko : new makeChar('Zuko', 'assets/images/zuko.png', 80, 30, 12),
-		toph : new makeChar('Toph', 'assets/images/toph.png', 100, 25, 10),
-		azula : new makeChar('Azula', 'assets/images/azula.png', 70, 35, 20)
-	};
+	var characters;
 
 	var charArray; // array of character objects
 	var remainingChars;
 	var hero; // character chosen by user
 	var opponent; 
 	var nextChar; //used for generating divs with characters in them; passed into displayChar()
+	var baseAttack;
 
 
 /********************************************
@@ -27,17 +21,26 @@ $(document).ready(function() {
 ********************************************/
 	startGame();
 
+
 	function startGame(){
 		// For restarting game
+		characters = {
+			aang : new makeChar('Aang', 'assets/images/aang.png', 295, 10, 15),
+			katara : new makeChar('Katara', 'assets/images/katara.png', 95, 40, 25),
+			sokka : new makeChar('Sokka', 'assets/images/sokka.png', 55, 20, 10),
+			zuko : new makeChar('Zuko', 'assets/images/zuko.png', 140, 35, 20),
+			toph : new makeChar('Toph', 'assets/images/toph.png', 135, 75, 50),
+			azula : new makeChar('Azula', 'assets/images/azula.png', 300, 5, 15)
+		};
 		$(".row2").empty();
-		$(".row3").empty();
 		$("#playAgain").css("display", "none");
+		partialReset();
 
 		// Set up characters
 		charArray = Object.keys(characters)
 		remainingChars = charArray;
 		for (i=0; i<charArray.length; i++) {
-			$("#instructions").html("Choose your character.");
+			$("#instructions").html("Choose your character.<br>Your character will have to defeat all others to win the game.");
 			$(".row2").append(displayChar(characters[charArray[i]].img, characters[charArray[i]].name, characters[charArray[i]].hp, characters[charArray[i]].attack, "pickHero"));
 		}
 	}
@@ -50,25 +53,33 @@ $(document).ready(function() {
 		return displayBlock;
 	};
 
+	function partialReset(){
+		$(".row3").empty();
+		$(".row3, #divider, #attackBtn").css("display", "none");
+	}
+
 /********************************************
 	CHOOSE CHARACTER
 *********************************************/
 	$(document).on("click", ".pickHero", function(){
 		hero = characters[$(this).attr("id")];
+		baseAttack = hero.attack;
 		$(".row2").empty();
 		$(".row2").append(displayChar(hero.img, hero.name, hero.hp, hero.attack, "hero"));
 		remainingChars.splice(remainingChars.indexOf(hero.name.toLowerCase()), 1);
 		$("#instructions").html("Choose your opponent.");
+		$(".row3").css("display", "block")
 		for (i=0; i<remainingChars.length; i++) {
 			$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "pickOpp"));
 		}
-		//$(this).blur(); //doesn't seem to affect anything
+		$("#divider").css("display", "block");
 	});
 
 /********************************************
 	CHOOSE OPPONENT
 *********************************************/
 	$(document).on("click", ".pickOpp", function(){
+		$(".opponent, #vs").remove();
 		opponent = characters[$(this).attr("id")];
 		$(".row3").empty();
 		$(".row2").append("<h3 id='vs'>VS</h3>");
@@ -88,42 +99,50 @@ $(document).ready(function() {
 		//Attack and change stats
 		opponent.hp -= hero.attack;
 		hero.hp -= opponent.counterAttack;
-		hero.attack += characters[hero.name.toLowerCase()].attack;
-		console.log("hi");
+		hero.attack += baseAttack;
+		$("#message").css("display", "block");
+		$("#message").append("<p>You deliver <strong>" + hero.attack + "</strong> damage to " + opponent.name + ".<br>"
+		 + opponent.name + " delivers <strong>" + opponent.counterAttack 
+		 + "</strong> damage to you.<br>You gain experience, increasing your attack by <strong>" + baseAttack + "</strong>.<br></p><p id='ok'>OK</p>");
 
-		// Reprint with new stat values
-		$(".row2").empty();
-		$(".row2").append(displayChar(hero.img, hero.name, hero.hp, hero.attack, "hero"));
-		$(".row2").append("<h3 id='vs'>VS</h3>");
-		$(".row2").append(displayChar(opponent.img, opponent.name, opponent.hp, opponent.counterAttack, "opponent"));
-		console.log("hi2")
+		$("#ok").on("click", function() {
+			$("#message").empty();
+			$("#message").css("display", "none");
+			
+
+			// Reprint with new stat values
+			$(".row2").empty();
+			$(".row2").append(displayChar(hero.img, hero.name, hero.hp, hero.attack, "hero"));
+			$(".row2").append("<h3 id='vs'>VS</h3>");
+			$(".row2").append(displayChar(opponent.img, opponent.name, opponent.hp, opponent.counterAttack, "opponent"));
 
 
-		// Lose condition
-		if (hero.hp<=0) {
-			$("#instructions").html(hero.name + " has no health left! You lose.");
-			$("#attackBtn").css("display", "none");
-			$("#playAgain").css("display", "inline-block");
-		}
-		else if (opponent.hp <= 0) {
-			$("#instructions").html(opponent.name + " has no health left! You win this round.<br>Pick a new opponent.");
-			$("#attackBtn").css("display", "none");
-			$(".row3").empty();
-			$(".opponent").remove();
-			$("#vs").remove();
-			for (i=0; i<remainingChars.length; i++) {
-				$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "pickOpp"));
+			// Lose condition
+			if (hero.hp<=0) {
+				$("#instructions").html(hero.name + " has no health left! You lose.");
+				$(".hero").fadeTo("fast",.5);
+				partialReset();
+				$("#playAgain").css("display", "inline-block");
 			}
-			console.log(remainingChars[0]);
-			console.log(opponent.hp);
-
-			// //Win condition
-			// if (remainingChars[0] ==='undefined' && opponent.hp <=0){
-			// 	$("#instructions").html("You have defeated all opponents! You win!");
-			// 	$("#attackBtn").css("display", "none");
-			// 	$("#playAgain").css("display", "inline-block");
-			// }
-		}
+			else if (opponent.hp <= 0) {
+				//Win condition
+				if ((remainingChars[0] === undefined) && (opponent.hp <= 0)){
+					partialReset();
+					$("#instructions").html("You have defeated all opponents! You win!");
+					$("#playAgain").css("display", "inline-block");
+				} 
+				// Keep playing until win/lose conditions are met
+				else {
+					$("#instructions").html(opponent.name + " has no health left! You win this round.<br>Pick a new opponent.");
+					partialReset();
+					$(".row3, #divider").css("display", "block");
+					$(".opponent").fadeTo("fast",.5);
+					for (i=0; i<remainingChars.length; i++) {
+						$(".row3").append(displayChar(characters[remainingChars[i]].img, characters[remainingChars[i]].name, characters[remainingChars[i]].hp, characters[remainingChars[i]].counterAttack, "pickOpp"));
+					}
+				}
+			}
+		});	
 
 	});
 
@@ -135,6 +154,22 @@ $(document).ready(function() {
 	});
 
 
-
+/********************************************
+	ANIMATION EFFECTS
+********************************************/
+	$(document).on({
+		mouseenter : function() {
+		$(this).fadeTo("fast", .5);
+		$("html, body").css("cursor", "pointer");
+		},
+		mouseleave : function(){
+		$(this).fadeTo("fast", 1);
+		$("html, body").css("cursor", "default");
+		},
+		click : function(){
+		$(this).fadeTo("fast", 1);
+		$("html, body").css("cursor", "default");
+		}
+	}, ".pickOpp, .pickHero");
 
 });
